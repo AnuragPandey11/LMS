eb46f246b835c38266dd017520b8396c1c2e0d16
GrayHat <37294843+GrayHat12@users.noreply.github.com>
2019-03-29 20:03:29 +0530

    Added Launcher

--- src/lms/LMS.java
+++ src/lms/LMS.java
@@ -5,18 +5,64 @@
  */
 package lms;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author GrayHat
  */
-public class LMS {
+public class LMS extends JFrame implements ActionListener{
 
+    JButton stdt=new JButton("STUDENT");
+    JButton facty=new JButton("FACULTY");
+    
+    LMS()
+    {
+        initContents();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        StudentMainScreen.main(new String[1]);
-        FacultyMainScreen.main(new String[1]);
+        new LMS().setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src=e.getSource();
+        if(src==stdt)
+        {
+            dispose();
+            StudentMainScreen.main(new String[1]);
+        }
+        else
+        {
+            try {
+                dispose();
+                FacultyMainScreen.main(new String[1]);
+            } catch (Exception ex) {
+                Logger.getLogger(LMS.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void initContents() {
+        stdt.setBounds(80, 80, 180, 30);
+        stdt.addActionListener(this);
+        facty.setBounds(80,120,180,30);
+        facty.addActionListener(this);
+        add(stdt);
+        add(facty);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setSize(400,400);
+        setLocation(500,300);
+        setContentPane(getContentPane());
     }
     
 }
